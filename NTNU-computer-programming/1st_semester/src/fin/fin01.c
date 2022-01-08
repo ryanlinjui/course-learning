66406a3b4b810df34fb2ad3dc08724e175f8f135
ryanlinjui <ryanlinjui@gmail.com>
2022-01-08 16:29:10 +0800

    final

--- NTNU-computer-programming/1st_semester/src/fin/fin01.c
+++ NTNU-computer-programming/1st_semester/src/fin/fin01.c
@@ -3,6 +3,8 @@
 #include <inttypes.h>
 #include <stdlib.h>
 
+double eq_a=0,eq_b=0,eq_c=0;
+
 int main()
 {
     printf("$ ./fin01\n");
@@ -17,12 +19,44 @@ int main()
         {
             if((p[i][0]==p[i-1][0])&&(p[i][1]==p[i-1][1]))
             {
-                printf("")
+                printf("This is not a pentagon\n");
+                exit(0);
             }
+            if(i==1)
+            {
+                eq_a = 1/((double)p[i][0]-(double)p[i-1][0]);
+                eq_b = -(1/((double)p[i][1]-(double)p[i-1][1]));
+                eq_c = -(-(double)p[i-1][1]/((double)p[i][1]-(double)p[i-1][1]))+(-(double)p[i-1][0]/((double)p[i][0]-(double)p[i-1][0]));
+            }
+        }
+        if(i>1)
+        {
+            double _eq_a = 1/((double)p[i][0]-(double)p[i-1][0]);
+            double _eq_b = -(1/((double)p[i][1]-(double)p[i-1][1]));
+            double _eq_c = -(-(double)p[i-1][1]/((double)p[i][1]-(double)p[i-1][1]))+(-(double)p[i-1][0]/((double)p[i][0]-(double)p[i-1][0]));
+            if((eq_a==_eq_a)&&(eq_b==_eq_b)&&(eq_c==_eq_c))
+            {
+                printf("This is not a pentagon\n");
+                exit(0);
+            }
+            eq_a = _eq_a;
+            eq_b = _eq_b;
+            eq_c = _eq_c;
         }
     }
 
-    
-
+    double area = 0;
+    area = ( ((double)p[0][0]*(double)p[1][1]-(double)p[1][0]*(double)p[0][1])+
+            ((double)p[1][0]*(double)p[2][1]-(double)p[2][0]*(double)p[1][1])+
+            ((double)p[2][0]*(double)p[3][1]-(double)p[3][0]*(double)p[2][1])+
+            ((double)p[3][0]*(double)p[4][1]-(double)p[4][0]*(double)p[3][1])+
+            ((double)p[4][0]*(double)p[0][1]-(double)p[0][0]*(double)p[4][1])
+            )/2;
+    if(area==0)
+    {
+        printf("This is not a pentagon\n");
+        exit(0);   
+    }
+    printf("%lf\n",area);
     return 0;
 }
---
+++ NTNU-computer-programming/1st_semester/src/fin/go.c
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+int32_t max_black_chain( int32_t board[][19])
+{
+    
+}
