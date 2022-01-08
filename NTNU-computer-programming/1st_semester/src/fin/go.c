2bb19e7c5200f35f104b07a678abc82a4d26075d
ryanlinjui <ryanlinjui@gmail.com>
2022-01-08 16:57:30 +0800

    final

--- NTNU-computer-programming/1st_semester/src/fin/go.c
+++ NTNU-computer-programming/1st_semester/src/fin/go.c
@@ -1,6 +1,63 @@
 #include <stdint.h>
+#include <stdio.h>
 
 int32_t max_black_chain( int32_t board[][19])
 {
+    int32_t b_black[19][19];
+    int32_t num=0;
+    for (int i = 0; i <19;i++)
+    {
+        for (int j = 0; j <19;j++)
+        {
+            b_black[i][j] = 0;
+        }
+    }
     
+    for (int i = 0; i <19;i++)
+    {
+        for (int j = 0; j <19;j++)
+        {
+            if(board[i][j]==1)
+            {
+                if(i+1!=19)
+                {
+                    if(board[i+1][j]==0)
+                    {
+                        b_black[i+1][j] = 1;
+                    }
+                }
+                if(i-1!=-1)
+                {
+                    if(board[i-1][j]==0)
+                    {
+                        b_black[i-1][j] = 1;
+                    }
+                }
+                if(j+1!=19)
+                {
+                    if(board[i][j+1]==0)
+                    {
+                        b_black[i][j+1] = 1;
+                    }
+                }
+                if(j-1!=-1)
+                {
+                    if(board[i][j-1]==0)
+                    {
+                        b_black[i][j-1] = 1;
+                    }
+                }
+            }
+        }
+    }
+    
+    for (int i = 0; i <19;i++)
+    {
+        for (int j = 0; j <19;j++)
+        {
+            num += b_black[i][j];
+        }
+    }
+
+    return num;
 }
