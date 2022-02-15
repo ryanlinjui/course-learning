b07c96093d21cfb293368db2fdf4381aa7016900
ryanlinjui <ryanlinjui@gmail.com>
2022-02-15 09:34:57 +0800

    [finish]/test all c files

--- NTNU-computer-programming/test.sh
+++ NTNU-computer-programming/test.sh
@@ -1,7 +1,16 @@
-test_dir="hw01"
-cd 1st
-cd $test_dir
-make clean
+test_dir="hw04"
+cd 1st/${test_dir}
 make
-./hw0101
-make clean
+
+function execute(){
+    echo ""
+    echo "========================"
+    ./$1
+    echo "========================"
+}
+
+for file in $(ls | grep ^[^.]*$ | grep ^[^Makefile]); do
+    execute $file
+done
+
+# make clean
