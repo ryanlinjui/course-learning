922ec49aa23b1f23a6fae2469d4a8a8d8b17c221
ryanlinjui <ryanlinjui@gmail.com>
2022-03-15 11:52:49 +0800

    finish

--- NTNU-computer-programming/2nd/hw02/hw0202.c
+++ NTNU-computer-programming/2nd/hw02/hw0202.c
@@ -1,18 +1,56 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
-#define MAX_LEN 1000 
+#define SIGN_BIT 1
+#define EXPONENT_BIT 11
+#define FRACTION_BIT 52
+
+void print_bin(uint64_t num,uint64_t num_exp)
+{   
+    for(int i=num_exp-1;i>=0;i--)
+    {
+        if((num>>i)&1)
+        {
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
+    }
+    printf("\n");
+}
+
+union
+{    
+    double num;
+    struct
+    {
+        uint64_t fraction : FRACTION_BIT;
+        uint64_t exponent : EXPONENT_BIT;
+        uint64_t sign : SIGN_BIT;
+    }element;
+}ieee;
 
 int main()
 {
-    char *num = malloc(MAX_LEN);
     printf("$ ./hw0202\n");
     printf("Please enter a floating-point number (double precision): ");
-    fgets(num, MAX_LEN, stdin);
-    for (int i = 0; i <strlen(num)-1; i++)
+    scanf("%lf",&ieee.num);
+    printf("Sign: %x\n",ieee.element.sign);
+    printf("Exponent: ");
+    print_bin(ieee.element.exponent,EXPONENT_BIT);
+    printf("Fraction: ");
+    print_bin(ieee.element.fraction,FRACTION_BIT);
+    printf("%g = (-1)^%d * (1",ieee.num,ieee.element.sign);
+    for(int i=FRACTION_BIT-1;i>=0;i--)
     {
-        
+        if((ieee.element.fraction>>i)&1)
+        {
+            printf(" + 2^%d",i-FRACTION_BIT);
+        }
     }
-    
+    printf(") * 2^(%u-%u)\n",ieee.element.exponent,(1<<(EXPONENT_BIT-1))-1);
     return 0;
 }
